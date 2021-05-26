f7b6085ca99510dab24721c7120b51974a891e12
karina <71703498+karinaaduu@users.noreply.github.com>
2021-05-27 02:21:10 +0300

    Add files via upload

---
+++ lab7/C#/Exeptions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExeptionsLibrary
+{
+    public class Exeptions
+    {
+        private double _a, _c, _d;
+        public double Computation { get; private set; }
+        public Exeptions(double a, double c, double d)
+        {
+            _a = a;
+            _c = c;
+            _d = d;
+        }
+        public void Exp()
+        {
+            if (_a == 0)
+                throw new DivideByZeroException();
+            if (_a >= 4)
+                throw new ArgumentException();
+            Computation = Func();
+        }
+        private double Func()
+        {
+            return ((2 * _c - _d / 23) / Math.Log(1 - _a / 4));
+        }
+        public (double, double, double) GetArguments
+        { get { return (_a, _c, _d); } }
+    }
+}
---
+++ lab7/C#/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ExeptionsLibrary;
+
+namespace proga7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Exeptions[] obj_arr = new Exeptions[4];
+            obj_arr[0] = new Exeptions(3, 2, 1); // right
+            obj_arr[1] = new Exeptions(2.75, 34.2, -8.274); // right
+            obj_arr[2] = new Exeptions(0, 23, -7); // devision by 0 (a=0)
+            obj_arr[3] = new Exeptions(10, 11, 12); // wrong  (a>4)
+            for (int i = 0; i < 4; i++)
+            {
+                try
+                {
+                    obj_arr[i].Exp();
+                    Console.WriteLine($"Evaluating an expression {i} for {obj_arr[i].GetArguments}: {obj_arr[i].Computation}");
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine($"Evaluating an expression {i} for {obj_arr[i].GetArguments}: Devision by zero. Input correct values.");
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine($"Evaluating an expression {i} for {obj_arr[i].GetArguments}: Wrong arguments. Input correct values.");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
