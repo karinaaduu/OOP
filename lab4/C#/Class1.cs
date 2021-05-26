c649902c86e0c39a4a1c9cb463553cfeb1398fad
karina <71703498+karinaaduu@users.noreply.github.com>
2021-05-27 02:37:57 +0300

    Add files via upload

---
+++ lab4/C#/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace VectorLibrary
+{
+    public class Vector
+    {
+        private double _x, _y;
+        public double Length { get; set; }  //властивість, яка дозволяє записати в себе довжину вектора у класі та отримати це значення
+        public double X { get { return _x; } }          //властивість для отримання координати абсциси
+        public double Y { get { return _y; } }          //властивість для отримання координати ординати
+        public Vector(double x, double y)  //конструктор з параметрами
+        {
+            _x = x;
+            _y = y;
+        }
+        public Vector()                 //конструктор за замовчуванням
+        {
+            _x = 1;
+            _y = 1;
+        }
+        public Vector(Vector v)             //конструктор копіювання
+        {
+            _x = v.X;
+            _y = v.Y;
+            Length = v.Length;
+        }
+        public void CalculateLength()                   //розрахунок довжини вектора
+        {
+            Length = Math.Sqrt((Math.Pow(_x, 2) + Math.Pow(_y, 2)));
+        }
+        public void Information()               //інформація щодо вектора
+        {
+            CalculateLength();
+            Console.WriteLine($"\nCoordinates: ({_x};{_y})");
+            Console.WriteLine($"Length: {Length}");
+        }
+        public static Vector operator *(int n, Vector v1)
+        {
+            Vector v = new Vector(n * (v1.X), n * (v1.Y));
+            return v;
+        }
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            Vector v = new Vector(v1.X - v2.X, v1.Y - v2.Y);
+            return v;
+        }
+    }
+}
---
+++ lab4/C#/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using VectorLibrary;
+
+namespace laba4
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            Console.WriteLine("Input coordinates (x;y): ");
+            x= Convert.ToInt32(Console.ReadLine());
+            y =Convert.ToInt32(Console.ReadLine());
+            Vector V1 = new Vector();
+            Vector V2 = new Vector(x,y);
+            Vector V3 = new Vector(V1);
+            Console.WriteLine("All vectors: ");
+            V1.Information();
+            V2.Information();
+            V3.Information();                          //copy vector V1
+            V1 = V3 - V2;
+            Console.WriteLine("\n----------------\nVector V1=V3-V2: ");
+            V1.Information();
+            Console.WriteLine("\n----------------\nVector V3=2*V3: ");
+            V3 = 2 * V3;
+            V3.Information();
+        
+            Console.ReadKey();
+
+        }
+    }
+}
