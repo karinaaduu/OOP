8f026a153db02bab0b379665a9da7348f5d9059c
karina <71703498+karinaaduu@users.noreply.github.com>
2021-05-27 02:41:00 +0300

    Add files via upload

---
+++ lab6/C#/SmallLetters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using RyadkiLibrary;
+
+namespace SmallLettersLibrary
+{
+    public class SmallLetters:Ryadki
+    {
+        public SmallLetters(string line) : base(line)
+        { }
+        public override int RyadokLength()
+        {
+            return str.Length;
+        }
+        override public void InsertSymbols()
+        {
+            string newstr = "";
+            for (int i = 1; i < str.Length; i += 2)
+            {
+                newstr = str.Insert(i, "\\");
+                str = newstr;
+            }
+        }
+    }
+}
